68bcad58e468f1e0e67b4305089a56b160be7dbe
carmencitaap <cmavendano@miuandes.cl>
2023-06-03 21:53:06 -0400

    feat: import of components

--- app1/src/App.tsx
+++ app1/src/App.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import './App.css';
+import Group from './Group'
+import Evaluation from './Evaluation';
 
 function App() {
   return (
@@ -10,46 +12,10 @@ function App() {
         <a href="#"> Evaluaciones </a> */}
       </div>
 
-
-
       <div className="main">
         <div className="container">
-          <div>
-            <span className="group-title"> Groups </span>
-              <div className="groups">
-                  <div> Group 1 </div>
-                  <div> Group 2 </div>
-              </div>
-          </div>
-          <div>
-            <span className="evaluations-title"> Evaluations </span>
-              <table className="evaluations">
-                  <thead>
-                      <tr>
-                          <th> Number </th>
-                          <th> Name </th>
-                          <th> Group </th>
-                      </tr>
-                  </thead>
-                  <tbody>
-                      <tr>
-                          <td> 1 </td>
-                          <td> Evaluacion 1</td>
-                          <td> Grupo 1</td>
-                      </tr>
-                      <tr>
-                          <td> 2 </td>
-                          <td> Evaluacion 2</td>
-                          <td> Grupo 2 </td>
-                      </tr>
-                      <tr>
-                          <td> 3 </td>
-                          <td> Evaluacion 3</td>
-                          <td> Grupo 3</td>
-                      </tr>
-                  </tbody>
-              </table>
-          </div>
+          <Group />
+          <Evaluation />
         </div>
       </div>
     </div>
