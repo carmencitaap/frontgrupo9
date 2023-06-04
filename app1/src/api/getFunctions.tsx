47c1570b649adb2e421d916378c0527351bf177c
carmencitaap <cmavendano@miuandes.cl>
2023-06-04 17:53:07 -0400

    feat: show ids

--- app1/src/api/getFunctions.tsx
+++ app1/src/api/getFunctions.tsx
@@ -1,10 +1,10 @@
 import React, {useState , useEffect} from 'react';
 
 function GetGroups() {
-    const [groups, setGroups] = useState([]);
 
+    const [groups, setGroups] = useState([]);
     const getGroups = () => {
-        fetch("https://cavenpal.pythonanywhere.com/group/")
+        fetch("https://cavenpal.pythonanywhere.com/group/", {method:'GET'})
         .then((response) => response.json())
         .then(data => {
             console.log(data);
@@ -15,22 +15,34 @@ function GetGroups() {
         })
       }
       useEffect (() => {
-        getGroups()
+        getGroups()   
       }, [])
-    
+
+
     return (
         <div className="groups">
+            {/* <div>
+                <span>{person !== null && (
+                    <span> Name: {person['name']}</span>  
+                )}
+                </span>
+            </div> */}
             {groups.map(group => (
                 <div key={group['id']}>
                     <span> Name: {group['name']} </span>
                     <br />
                     <span> Number of people: {group['number_of_people']} </span>
                     <br />
-                    <span> People: {group['people']} </span>
+                    {/* <span> People: {group['people']} </span> */}
+                    <span>
+                        People: {(group['people'] as any[]).map((personId) => personId).join(", ")} 
+                    </span>
                 </div>
             ))}
         </div>
     );
 }
 
-export default GetGroups;
+
+
+export default GetGroups; 
