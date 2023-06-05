61ed5d09163fe5bf89d4d201e77b0c7345a2d855
carmencitaap <cmavendano@miuandes.cl>
2023-06-05 15:18:29 -0400

    feat: Create a question

--- app1/src/api/functionsQ.tsx
+++ app1/src/api/functionsQ.tsx
@@ -1,5 +1,6 @@
 import React, {useState , useEffect} from 'react';
-
+import Popup from 'reactjs-popup';
+import CreateButton from '../components/Button';
 function GetQuestions(){
     const [questions, setQuestions] = useState([]);
 
@@ -19,9 +20,73 @@ function GetQuestions(){
         getQuestions()
     })
 
+
+    const addQuestion = async (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        
+        const question = (document.getElementById('question') as HTMLInputElement).value;
+        const order = (document.getElementById('order') as HTMLInputElement).value;
+        const correct_answer = (document.getElementById('correct-answer') as HTMLInputElement).value;
+        const type_question = (document.getElementById('type-question') as HTMLInputElement).value;
+        const difficulty = (document.getElementById('difficulty') as HTMLInputElement).value;
+        const tags = (document.getElementById('tags') as HTMLInputElement).value;
+        const evaluation = (document.getElementById('evaluation') as HTMLInputElement).value;
+
+        await fetch("https://cavenpal.pythonanywhere.com/question/", {
+            method: 'POST',
+            body: JSON.stringify({
+                order: order,
+                question: question,
+                correct_answer: correct_answer,
+                type_question: type_question,
+                difficulty: difficulty,
+                tags_question: tags,
+                evaluation: evaluation
+            }),
+            headers: {
+                'Content-type': 'application/json; charset=UTF-8',
+            },
+        })
+        console.log(order,question,correct_answer,type_question,difficulty,tags,evaluation);
+    };
+
+
     return (
         <div>
-            <span className="question-title"> Questions </span>
+            <div id="person-create" className = "add-img">
+                <span className="question-title"> Questions </span>
+                <Popup trigger={<img src="add-row-svgrepo-com.svg" alt=""/>}
+                position="right center">
+                    <div className="popup">
+                        <span className="question-popup"> Create a question! </span>
+                        <form onSubmit={addQuestion}>
+                            <label htmlFor="question"> Question: </label>
+                            <input type="text" id="question" name="question" /><br />
+
+                            <label htmlFor="order">Order: </label>
+                            <input type="text" id="order" name="order" /><br />
+
+                            <label htmlFor="correct-answer">Correct Answer: </label>
+                            <input type="text" id="correct-answer" name="correct-answer" /><br />
+
+                            <label htmlFor="type-question"> Type: </label>
+                            <input type="text" id="type-question" name="type-question" /><br />
+
+                            <label htmlFor="difficulty"> Difficulty: </label>
+                            <input type="text" id="difficulty" name="difficulty" /><br />
+
+                            <label htmlFor="tags"> Tags: </label>
+                            <input type="text" id="tags" name="tags" /><br />
+
+                            <label htmlFor="evaluation"> Evaluation: </label>
+                            <input type="text" id="evaluation" name="evaluation" /><br />
+
+                            <CreateButton />
+
+                        </form>
+                    </div>
+                </Popup>
+            </div>
             <table className="questions">
                 <thead>
                     <tr>
