196793de0894346b53873f39637d67e849b93a1c
carmencitaap <cmavendano@miuandes.cl>
2023-06-09 21:38:06 -0400

    feat: Add person to group

--- app1/src/api/functionsGr.tsx
+++ app1/src/api/functionsGr.tsx
@@ -3,8 +3,11 @@ import React, {useState , useEffect} from 'react';
 import Popup from 'reactjs-popup';
 import CreateButton from '../components/Button';
 
-function GetGroups() {
+const GROUP_ENDPOINT = "https://cavenpal.pythonanywhere.com/group/"
+const PERSON_ENDPOINT = "https://cavenpal.pythonanywhere.com/person/"
 
+
+function GetGroups() {
     const [groups, setGroups] = useState([]);
     const getGroups = () => {
         fetch("https://cavenpal.pythonanywhere.com/group/", {method:'GET'})
@@ -21,29 +24,96 @@ function GetGroups() {
         getGroups()   
       }, [])
     
-      const addGroup = async (event: React.FormEvent<HTMLFormElement>) => {
+    const addGroup = async (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault();
       
         const name = (document.getElementById('name') as HTMLInputElement).value;
         const number_of_people = (document.getElementById('np') as HTMLInputElement).value;
-        let people = (document.getElementById('people') as HTMLInputElement).value;
-        let people_array = people.split(",")
 
 
-        await fetch("https://cavenpal.pythonanywhere.com/group/", {
+        await fetch(GROUP_ENDPOINT, {
             method: 'POST',
             body: JSON.stringify({
                 name: name,
-                number_of_people: number_of_people,
-                people: people_array
+                number_of_people: number_of_people
             }),
             headers: {
                 'Content-type': 'application/json; charset=UTF-8',
             },
         })
 
-        console.log(name,number_of_people,people)
+        console.log(name,number_of_people)
     };
+
+    const addPerson = async (id: any) => {
+        const full_name = (document.getElementById('name') as HTMLInputElement).value;
+        const email = (document.getElementById('email') as HTMLInputElement).value;
+
+        await fetch(PERSON_ENDPOINT, {
+            method: 'POST',
+            body: JSON.stringify({
+                full_name: full_name,
+                email: email,
+                group_id: id
+            }),
+            headers: {
+                'Content-type': 'application/json; charset=UTF-8',
+            },
+        })
+        console.log(full_name,email)
+    };
+
+    return (
+            <div className="groups">
+                <div id="group-create">
+                <Popup trigger={<img src="plus-solid.svg" alt="" className="margin"/>}
+                position="right center">
+                    <div className="popup">
+                        <span className="group-popup"> Create a group! </span>
+                        <form onSubmit={addGroup}>
+                            <label htmlFor="name">Name: </label>
+                            <input type="text" id="name" name="name" /><br />
+
+                            <label htmlFor="np">Number of people: </label>
+                            <input type="text" id="np" name="np" /><br />
+
+                            <CreateButton />
+                        </form>
+                    </div>
+                </Popup>
+                </div>
+
+            {groups.map(group => (
+                <div key={group['id']}>
+                    <span> Group ID: {group['id']}</span>
+                    <br />
+                    <span> Name: {group['name']} </span>
+                    <br />
+                    <span> Number of people: {group['number_of_people']} </span>
+                    <br />
+                    <Popup trigger={<img src="user-plus-solid.svg" alt=""/>}
+                    position="right center">
+                        <div className="popup">
+                            <span className="person-popup"> Create a person! </span>
+                            <form onSubmit={()=>addPerson(group['id'])}>
+                                <label htmlFor="name"> Full Name: </label>
+                                <input type="text" id="name" name="name" /><br />
+
+                                <label htmlFor="email">Email: </label>
+                                <input type="text" id="email" name="email" /><br />
+
+                                <CreateButton />
+                            </form>
+                        </div>
+                    </Popup>
+                </div>
+            ))}
+        </div>
+    );
+}
+
+export default GetGroups;
+
 
     // const [people, setPeople] = useState([]);
     // const getPeople = (groupId: string | undefined) => {
@@ -74,64 +144,3 @@ function GetGroups() {
     // useEffect(() => {
     //     getPerson('1')
     // }, [])
-
-
-    return (
-            <div className="groups">
-                <div id="group-create">
-                <Popup trigger={<img src="plus-solid.svg" alt="" className="margin"/>}
-                position="right center">
-                    <div className="popup">
-                        <span className="group-popup"> Create a group! </span>
-                        <form onSubmit={addGroup}>
-                            <label htmlFor="name">Name: </label>
-                            <input type="text" id="name" name="name" /><br />
-
-                            <label htmlFor="np">Number of people: </label>
-                            <input type="text" id="np" name="np" /><br />
-
-                            <label htmlFor="people">People: </label>
-                            <input type="text" id="people" name="people" /><br />
-
-                            <CreateButton />
-                        </form>
-                    </div>
-                </Popup>
-                </div>
-            {groups.map(group => (
-                <div key={group['id']}>
-                    <span> Name: {group['name']} </span>
-                    <br />
-                    <span> Number of people: {group['number_of_people']} </span>
-                    <br />
-                    {/* <span> People: {group['people']} </span> */}
-                    <span>
-                        People: {(group['people'] as any[]).map((personId) => personId).join(", ")} 
-                    </span>
-                </div>
-                
-            ))}
-
-
-        </div>
-    );
-}
-
-// function createGroup() {
-//     const createGroup = (name, number_of_people, people) => {
-//         await fetch("https://cavenpal.pythonanywhere.com/group/", {
-//             method: 'GET',
-//             body: JSON.stringify({
-//                 name: name,
-//                 number_of_people: null,
-//                 people: []
-//             })
-//         })
-//     }
-//  }
-
-
-
-
-
-export default GetGroups;
