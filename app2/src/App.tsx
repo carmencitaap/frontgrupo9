92dceebf47cc3e5547d5098ee8d5f101415676a7
Victoria Guerriero <vaguerriero@miuandes.cl>
2023-06-11 22:44:37 -0400

    feat: created FinishedTest and added to App

--- app2/src/App.tsx
+++ app2/src/App.tsx
@@ -3,6 +3,7 @@ import './static/App.css';
 import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import EvaluationView from './components/EvaluationView';
 import QuestionView from './components/QuestionView';
+import FinishedTest from './components/FinishedTest';
 
 function App() {
   return (
@@ -12,9 +13,9 @@ function App() {
           <span className="home-title">Answer Evaluation </span>
         </div>
       </div> */}
-      {/* <Routes>
-        <Route path="/" element={<AnswerTest />}/>
-      </Routes> */}
+      <Routes>
+        <Route path="/finish" element={<FinishedTest />}/>
+      </Routes>
 
       <Routes>
             <Route path="/evaluation/:evaluationId" element={<EvaluationView />}></Route>
---
+++ app2/src/components/FinishedTest.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function FinishedTest(){
+    return (
+        <div className="finished">
+            <h3 className="finished-title">Congratulations!</h3>
+            <p>You finished the test.</p>
+            <img src="happy-gray-cat-celebration-party-dancing-9qc6ss84qhamexxg.gif" alt="Cat gif" className="image-size"></img>
+        </div>
+    )
+}
+
+export default FinishedTest;
