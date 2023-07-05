a96437df33d834c8730314fddb0163182d28f69e
carmencitaap <cmavendano@miuandes.cl>
2023-07-05 07:13:41 -0400

    fix: redirect links

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ function App() {
         </div>
       </div> */}
       <Routes>
-        <Route path="/finish/:score" element={<FinishedTest />}/>
+        <Route path="/finish/" element={<FinishedTest />}/>
       </Routes>
 
       <Routes>
